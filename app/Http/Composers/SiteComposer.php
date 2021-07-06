41c6d170bd524c395ca7e3411d11171cc61a6d79
Chris Deaton <dcdeaton@ncsu.edu>
2021-07-05 21:34:58 -0400

    implement site view composer
    so we don't have to include this logic everywhere

--- app/Http/Composers/SiteComposer.php
+++ app/Http/Composers/SiteComposer.php
@@ -5,13 +5,20 @@ namespace App\Http\Composers;
 
 use App\Support\RssReader;
 use App\Support\Twitter;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Http;
 use Illuminate\View\View;
+
 
 class SiteComposer
 {
     public function compose(View $view)
     {
+        if (App::environment('testing')) {
+            $this->handleTestingEnvironment($view);
+            return;
+        }
+
         /**
          * non-cached data
          */
@@ -40,8 +47,27 @@ class SiteComposer
                     ->acceptJson()
                     ->get(config('services.aod.tracker_url')."/api/v1/divisions")
                     ->json('data') ?? [];
+
         } catch (\Exception $exception) {
             return [];
         }
+    }
+
+    /**
+     * Prevent talking out in testing
+     *
+     * @param  View  $view
+     */
+    private function handleTestingEnvironment(View $view)
+    {
+        $view->with('aod_divisions',cache()->remember('aod_divisions', 300, function () {
+            return json_decode(file_get_contents(storage_path('testing/divisions.json')), true)['data'];
+        }));
+
+        $view->with('aod_tweets', json_decode(file_get_contents(storage_path('testing/tweets.json'))));
+
+        $view->with('aod_announcements',
+            simplexml_load_string(file_get_contents(storage_path('testing/announcements.xml')))
+        );
     }
 }
