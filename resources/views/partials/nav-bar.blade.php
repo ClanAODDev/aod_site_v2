029424c2cbbb900686f684c35c0fef9f3d4a987a
Chris Deaton <dcdeaton@ncsu.edu>
2021-07-07 05:48:33 -0400

    fix navigation bug

--- resources/views/partials/nav-bar.blade.php
+++ resources/views/partials/nav-bar.blade.php
@@ -16,13 +16,7 @@
         </a>
         <div class="hamburger"><i class="fa fa-bars fa-2x"></i></div>
         <div class="nav-items">
-            <ul id="menu-primary" class="menu">
-                <li class="home show-logo">
-                    <a href="/" class="text-link">Home</a>
-                    <img class="clan-logo" alt="AOD Clan Logo"
-                         src="{{ asset('images/aod_new.png')}}"
-                         onclick="window.location.replace('/')">
-                </li>
+            <ul id="menu-mobile" class="menu">
                 <li id="menu-item-269" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-269"><a
                         href="/forums">Forums</a></li>
                 <li><a href="https://www.clanaod.net/divisions/">Divisions</a></li>
