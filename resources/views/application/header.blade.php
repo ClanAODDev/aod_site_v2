7651b99076fb920a8fd5209f976bb61817d969e3
Chris Deaton <dcdeaton@ncsu.edu>
2021-07-11 15:11:30 -0400

    Cache bust for frontend assets

--- resources/views/application/header.blade.php
+++ resources/views/application/header.blade.php
@@ -1,8 +1,8 @@
 <head>
     <title>ClanAOD.net | @yield('page-title', 'Since 1999')</title>
 
-    <link rel="stylesheet" href="{{ asset('css/app.css?v=2.2') }}"/>
-    <link rel="stylesheet" href="{{ asset('css/fonts.css?v=2.2') }}"/>
+    <link rel="stylesheet" href="{{ asset('css/app.css?v=2.3') }}"/>
+    <link rel="stylesheet" href="{{ asset('css/fonts.css?v=2.3') }}"/>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/solid.min.css" integrity="sha512-jQqzj2vHVxA/yCojT8pVZjKGOe9UmoYvnOuM/2sQ110vxiajBU+4WkyRs1ODMmd4AfntwUEV4J+VfM6DkfjLRg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" integrity="sha
