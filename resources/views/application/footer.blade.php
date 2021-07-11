7af7171b4ed46d772cb281f60a70f1205efa0e01
Chris Deaton <dcdeaton@ncsu.edu>
2021-07-11 17:09:01 -0400

    Extract tweets partial

--- resources/views/application/footer.blade.php
+++ resources/views/application/footer.blade.php
@@ -2,11 +2,13 @@
     <div class="section-content-container">
         <div class="section-blurb">
             <div class="logo">
-                <img src="{{ asset('images/official-logo.png') }}"/>
+                <img src="{{ asset('images/official-logo.png') }}" alt="Clan AOD Logo"/>
             </div>
             <div class="blurb-text">
                 <h1>About The Angels of Death</h1>
-                <p>The Angels of Death is a community of players founded in 1999 based on a core set of conduct that aims to promote decency and provide a comfortable environment to play with thousands of other likeminded members.</p>
+                <p>The Angels of Death is a community of players founded in 1999 based on a core set of conduct that
+                    aims to promote decency and provide a comfortable environment to play with thousands of other
+                    likeminded members.</p>
                 <p>With nearly 3000 members globally there is always someone to play alongside when you want.</p>
             </div>
         </div>
@@ -15,22 +17,11 @@
     <div class="section-content-container">
         @include('partials.announcements')
         @include('partials.sitemap')
-
-        <div class="footer-section full-width twitter-feed">
-            <h1>Twitter Activity</h1>
-            <ul>
-                @foreach ($aod_tweets as $tweet)
-                    <li>
-                        <a href="https://twitter.com/officialclanaod" target="_blank">@officialclanaod</a>
-                        {!! urlify($tweet->text) !!}
-                    </li>
-                @endforeach
-            </ul>
-        </div>
+        @include('partials.tweets')
 
         <div class="site-meta footer-section full-width centered">
             <ul>
-                <li>Copyright &copy; <?php echo "1999 - ".date('Y'); ?> Angels of Death. All rights reserved.</li>
+                <li>Copyright &copy; <?php echo "1999 - " . date('Y'); ?> Angels of Death. All rights reserved.</li>
                 <li><a href="https://www.clanaod.net/privacy-policy/">Privacy Policy</a></li>
                 <li>-</li>
                 <li><a href="https://www.clanaod.net/terms-of-use/">Terms of Use</a></li>
---
+++ resources/views/partials/tweets.blade.php
@@ -0,0 +1,15 @@
+<div class="footer-section full-width twitter-feed">
+    <h1>Twitter Activity</h1>
+    @if ($tweets)
+        <ul>
+            @foreach ($aod_tweets as $tweet)
+                <li>
+                    <a href="https://twitter.com/officialclanaod" target="_blank">@officialclanaod</a>
+                    {!! urlify($tweet->text) !!}
+                </li>
+            @endforeach
+        </ul>
+    @else
+        <p>Could not load Twitter feed!</p>
+    @endif
+</div>
