a3a2e9bea071a318a5103dd45eacfc37a3a5ebc5
Chris Deaton <dcdeaton@ncsu.edu>
2021-07-23 08:50:21 -0400

    Update Call of Duty content
    [skip ci]

--- resources/views/division/content/call-of-duty.blade.php
+++ resources/views/division/content/call-of-duty.blade.php
@@ -1,12 +1,16 @@
 <div class="section-sub-section automenu">
-    <p>Welcome, Soldier and thank you for your interest in the Angels of Death!</p>
-    <p>The Angels of Death – Call of Duty Division currently supports War Zone, Modern Warfare, and Cold War. We’re a
-        gaming community that knows how to win but prefers good people over skill. Our main goal is to create a diverse,
-        friendly, and non-toxic environment for all Angels of Death members to play the games they enjoy.</p>
+
+    <p>Welcome, Soldier, and thank you for your interest in the Angels of Death!</p>
+
+    <p>The Angels of Death – Call of Duty Division supports Warzone, Modern Warfare, and Cold War. We’re a gaming
+        community that knows how to win but prefers good people over skill. Our goal is to create a diverse, friendly,
+        and non-toxic environment for all Angels of Death members to play the games they enjoy. We communicate, we play
+        clean, and we play as a team.</p>
+
     <p>The Call of Duty Division is home to members from all over the world with varying skill levels. No matter your
-        time zone or experience, there is always someone online to squad up with. Everyone here is united by our love
-        for gaming and the fun, mature, judgment free atmosphere we have created. So, if you’re looking for a community
-        with members that play with honor and respect then you are in the right place.</p>
+        time zone or level of experience, there is always someone online to squad up with. We are united by our love for
+        gaming and the fun, mature, judgment-free atmosphere we have created. If you’re looking for a community with
+        members that play with honor and respect, you're in the right place.</p>
 </div>
 
 <div class="section-sub-section automenu">
@@ -27,30 +31,12 @@
     <p>There are also AOD Exclusive Forum Badges and Awards that any member can earn through special events, in-game
         challenges, and tournaments.</p>
     <ul>
-        <li>Seasoned Officer (Officer Rank 500)</li>
+        <li>Seasoned Officer (Prestige Progression)</li>
         <li>Gilded Guns (Gun Camo Challenges)</li>
         <li>Answered The Call (Participation in Quarterly Events)</li>
         <li>Gunslinger (Quarterly Tournament Champions)</li>
-        <li>Verdansk Veteran (War Zone Wins)</li>
-        <li>With more to come!</li>
+        <li>Verdansk Veteran (Warzone Wins)</li>
     </ul>
-</div>
-
-<div class="section-sub-section automenu">
-    <h2 id="stack-up-and-wait-for-my-go">
-        Stack up and wait for my go
-    </h2>
-
-    <p>Down to throw a couple punches at your fellow comrade? Slam down into Spec Ops? Celebrate Easter early and go on
-        an Easter egg hunt?</p>
-    <p>Well have I got news for you! Here in the Call of Duty division we make it our mission to provide events that
-        will leave you wanting more. May that be a custom game of knives only on a Saturday night, a friendly lobby of
-        free-for-all versus your best buddies or gathering up troops to take on public lobbies in Ground War. Our
-        officers make it their priority to give you the best experience by hosting Call of Duty Invitationals, or by
-        giving you the chance to show your skill in a CWL inspired 5v5 tournament! These tournaments are available to
-        any Angel of Death ready to show their prowess on the front line.</p>
-    <p>The door is wide open soldier, so what are you waiting for?</p>
-
 </div>
 
 <div class="section-sub-section automenu">
@@ -68,4 +54,3 @@
         <li>Wear the AOD tags when playing Call of Duty</li>
     </ul>
 </div>
-
