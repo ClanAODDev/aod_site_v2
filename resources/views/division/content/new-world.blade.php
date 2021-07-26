486dad4711b4a270fa7101e4fee12d0a696dcd56
Chris Deaton <dcdeaton@ncsu.edu>
2021-07-26 16:59:05 -0400

    Add NW content

---
+++ resources/views/division/content/new-world.blade.php
@@ -0,0 +1,82 @@
+<div class="section-sub-section automenu">
+    <p>Fate has summoned you to the Angels of Death, New World Division.</p>
+
+    <p>This game is set on the fictional and supernatural island of Aeternum in the 17th century. The land of Aeternum
+        will be an evolving one that changes with the seasons, weather, and time of day.</p>
+
+    <p>Once you are a member of the New World Division you are welcome to explore a thrilling, open-world MMO filled
+        with danger and opportunity where you'll forge a new destiny for yourself alongside friends as you group up and
+        explore after being shipwrecked on the supernatural island of Aeternum. There will be endless opportunities to
+        fight, and forage among the island's wilderness and ruins. Channel supernatural forces or wield deadly weapons
+        in a classless, real-time combat system, and, fight with a small team, or in massed armies for PvE and PvP
+        battles—the choices are all yours.</p>
+
+    <p>Opportunities to overcome the brutal legions of The Corrupted and draw battle lines with competing players in
+        this land of danger await you. You can work with others to reclaim monster-haunted wilds and build new
+        civilizations. In a land hell-bent on your destruction, what will you do to survive? Band together with fellow
+        clanmates to forge, fortify, and fight your heroic path, or continue on as a lone wolf?</p>
+</div>
+
+<div class="section-sub-section automenu">
+    <h2 id="what-we-do">
+        Trailers
+    </h2>
+
+    <p>
+        <iframe class="youtube-embed" src="https://www.youtube.com/embed/Na4MWetfN8Q?feature=oembed"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen=""></iframe>
+    </p>
+
+    <p>
+        <iframe class="youtube-embed" src="https://www.youtube.com/embed/m1C5dLnvlzg?feature=oembed"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen=""></iframe>
+    </p>
+
+</div>
+
+<div class="section-sub-section automenu">
+    <h2 id="what-we-do">
+        Servers and Events
+    </h2>
+
+    <p>The division is currently found on Suddene Server.</p>
+    <p>Our community events range from scheduled Dungeon Adventures to impromptu fun; including both the regular game
+        modes and those we’ve engineered ourselves. We’re always looking for new and fun ways to keep things
+        interesting. As we progress in the game we will host end game content, ie 50v50 Territory battles, and
+        large-scale bracket competitions.</p>
+
+    <h3>Factions</h3>
+
+    <p>Currently, AOD is host to Companies in each faction so there is always a choice for which path is right for
+        you.</p>
+</div>
+
+
+<div class="section-sub-section automenu">
+    <h2 id="what-you-need-to-do">
+        Apply to division
+    </h2>
+
+    <p>In order to be eligible for membership in the New World division, you must meet the following criteria:</p>
+    <ul>
+        <li>Be at least 18 years of age</li>
+        <li>Be on TeamSpeak whenever you are in an AOD supported game</li>
+        <li>Wear AOD tags (as directed) in any title we support</li>
+        <li>Contribute to the forums on a bi-weekly basis to maintain activity</li>
+        <li>Have a Discord account and join our Discord server</li>
+        <li>Be a fan of extreme meme activity in Discord</li>
+        <li>No seriously, we have Gif wars...often.</li>
+    </ul>
+
+</div>
+
+
+
+
+
+
+
+
+
