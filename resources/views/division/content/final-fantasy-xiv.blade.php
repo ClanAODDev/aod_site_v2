ce01dec1a1fc6cd16e65fd1bf1269d0ee7e6dc8a
Chris Deaton <dcdeaton@ncsu.edu>
2021-09-06 16:48:12 -0400

    Add FFXIV content

---
+++ resources/views/division/content/final-fantasy-xiv.blade.php
@@ -0,0 +1,51 @@
+<div class="section-sub-section automenu">
+    <p>At AOD, your legacy can last for years ...</p>
+
+    <p>Hello and welcome to our home on the Crystal Data Center and the Brynhildr Server!
+        We're looking for new, veteran and returning players alike interested in mount / minion farms, map hunts,
+        chocobo racing, SE events, our own social FC events - honestly any content from ARR through to preparing for
+        Endwalker it's all on the table. And yes, we have members who do Savage content! Established in 2021, we have
+        intentionally cultivated a community focused on less drama and more gaming. Here your relationships and
+        investments within the community truly matter. And while victory is important to us all, we value the individual
+        first - not individual skill. There are no minimum item level (ilvl) or job level (jlvl) requirements - everyone
+        is welcome.</p>
+
+    <p>Easy requirements; 1) Be 16+ & 2) Have a working microphone.</p>
+</div>
+<div class="section-sub-section automenu">
+    <h2 id="servers-and-events">
+        FC Events & Badges
+    </h2>
+
+    <p>Our FC runs all content from ARR through to anything and everything current. We actively participate in
+        Square-Enix sponsored events and organize our own weekly and bi-weekly social events like mount and crafting
+        farms. And if you're a collector or a completionist, we've badges galore you can earn that'll make your forum
+        profile shine to go along with all those minions, mounts and that glorious end-game glamour.</p>
+
+    <p>Our goal is to make sure no one plays alone. Need a hand grinding out a new job? We got you. Need a friend to
+        help you with that Fate you want to clear? Again, we got you. Want someone to rage along with you about new raid
+        mechanics? Yep, we got you there too. In fact, we encourage anyone and everyone we find playing alone to party
+        up with our other members. No one beats a dungeon all by themselves nor should they. In AOD its about the
+        community and we'd like for you to be a part of it.</p>
+
+    </div>
+<div class="section-sub-section automenu">
+    <h2 id="apply-to-division">
+        Apply to division
+    </h2>
+
+    <p>If you're reading this you're halfway there. If you can meet the criteria below you're at 80%.</p>
+
+    <ul>
+        <li>Be a mature 16+ years of age</li>
+        <li>Have a functioning microphone</li>
+        <li>Participate socially on our forums</li>
+        <li>Be active in FFXIV once every 30 days</li>
+    </ul>
+
+    <p>We've no silly requirements like item level, job level or parses. Yeah, you read that right. All we're interested in is YOU.</p>
+
+    <p>The only thing left now is to click the 'Apply' button below. Well, what're you waiting for, Warrior of Light? Hydaelyn?</p>
+</div>
+
+
