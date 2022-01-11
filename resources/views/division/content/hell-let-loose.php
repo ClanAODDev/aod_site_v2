7239ff4b0fa08d23b38aab4bd1c8852f1cbf9f68
Chris Deaton <dcdeaton@ncsu.edu>
2022-01-11 16:43:41 -0500

    Add HLL content

--- resources/views/division/content/hell-let-loose.php
+++ resources/views/division/content/hell-let-loose.php
@@ -1,10 +1,32 @@
 <div class="section-sub-section automenu">
-    <p>Content coming soon!</p>
+    <p>Clan AOD's Hell Let Loose division is home to members from all over the world with varying skill levels. We are
+        united by our love for gaming and the fun, mature, non-toxic environment we have created. If you’re looking for
+        a community with members that play with honor and respect, you've come to the right place.</p>
+
+    <p>We communicate, we play clean, and we play as a team.</p>
 </div>
 <div class="section-sub-section automenu">
     <h2 id="servers-and-events">
         Servers and Events
     </h2>
 
-    <p>Content coming soon!</p>
+    <p>The division currently maintains one server for Hell Let Loose (AOD Stomping Grounds), located in US East (NY).
+        We run weekly events such as gaming nights, infantry training boot camps, armor crew certification, and
+        more!</p>
+</div>
+
+<div class="section-sub-section automenu">
+    <h2 id="servers-and-events">
+        Apply to division
+    </h2>
+
+    <p>In order to be eligible for membership in the Hell Let Loose division, you must meet the following criteria:</p>
+
+    <ul>
+        <li>Be at least 16 years of age</li>
+        <li>Be on TeamSpeak whenever you are in-game</li>
+        <li>Wear AOD clan tags (as directed) in any title we support</li>
+        <li>Cook your steaks in the microwave</li>
+        <li>Contribute to the forums on a monthly basis to maintain activity</li>
+    </ul>
 </div>
