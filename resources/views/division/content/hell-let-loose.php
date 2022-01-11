9958ab4e5810ba562d1ec33ca4511d17617c4f78
Chris Deaton <dcdeaton@ncsu.edu>
2022-01-11 13:29:49 -0500

    Add HLL content, icon

---
+++ resources/views/division/content/hell-let-loose.php
@@ -0,0 +1,10 @@
+<div class="section-sub-section automenu">
+    <p>Content coming soon!</p>
+</div>
+<div class="section-sub-section automenu">
+    <h2 id="servers-and-events">
+        Servers and Events
+    </h2>
+
+    <p>Content coming soon!</p>
+</div>
