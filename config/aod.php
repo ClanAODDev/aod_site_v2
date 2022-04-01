6f54bfe57368f0d0cd4489f38550b125bbd3f1f1
Chris Deaton <dcdeaton@ncsu.edu>
2022-04-01 18:01:24 -0500

    Update fallen

--- config/aod.php
+++ config/aod.php
@@ -7,44 +7,49 @@ return [
      */
     'fallen-angels' => [
         [
-            'name' => 'Kevin "Bluntz" Lovelace',
+            'name'          => 'Kevin "Bluntz" Lovelace',
             'date_of_death' => '11/14/2021',
             'forum_profile' => 'https://www.clanaod.net/forums/member.php?u=3419',
         ],
         [
-            'name' => 'AchesAndPains',
+            'name'          => 'AchesAndPains',
             'date_of_death' => '07/17/2020',
             'forum_profile' => 'https://www.clanaod.net/forums/member.php?u=33030',
         ],
         [
-            'name' => 'Bruce "hailhydra2018" Kennedy',
+            'name'          => 'Bruce "hailhydra2018" Kennedy',
             'date_of_death' => '01/18/2020',
             'forum_profile' => 'https://www.clanaod.net/forums/member.php?u=62669',
         ],
         [
-            'name' => 'MD9445',
+            'name'          => 'MD9445',
             'date_of_death' => 'Unknown',
             'forum_profile' => 'https://www.clanaod.net/forums/member.php?u=20244',
         ],
         [
-            'name' => 'Pafire',
+            'name'          => 'Pafire',
             'date_of_death' => 'Unknown',
             'forum_profile' => 'https://www.clanaod.net/forums/member.php?u=51680',
         ],
         [
-            'name' => 'Quake-id',
+            'name'          => 'Quake-id',
             'date_of_death' => 'Unknown',
             'forum_profile' => 'https://www.clanaod.net/forums/member.php?u=56057',
         ],
         [
-            'name' => 'Lance "Syph3n" Groth',
+            'name'          => 'Lance "Syph3n" Groth',
             'date_of_death' => 'Unknown',
             'forum_profile' => 'https://www.clanaod.net/forums/member.php?u=26433',
         ],
         [
-            'name' => 'oc675',
+            'name'          => 'oc675',
             'date_of_death' => '11/08/2015',
             'forum_profile' => 'https://www.clanaod.net/forums/member.php?u=35067',
+        ],
+        [
+            'name'          => 'Pafire',
+            'date_of_death' => '11/05/2020',
+            'forum_profile' => 'https://www.clanaod.net/forums/member.php?u=51680',
         ],
 
     ],
