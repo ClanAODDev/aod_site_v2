00bd003c99890fac82af8de224894b0caa4b445c
Chris Deaton <dcdeaton@ncsu.edu>
2022-04-01 17:53:43 -0500

    Battlefield content update

--- resources/views/division/content/battlefield.blade.php
+++ resources/views/division/content/battlefield.blade.php
@@ -1,12 +1,10 @@
 <div class="section-sub-section automenu">
-    <p>Clan AOD’s Battlefield Division is the largest division within the clan. We are firstmost a casual gaming group
-        and we accept players of all skill ranges. Respect for your fellow clan-mates and having a good time gaming
-        together are what we are all about. We endeavor to create a true community environment of good sportsmanship and
-        absolute fun. Additionally, our current and former members include: 5 DICE Friend Tags holders, 3 EA Game
-        Changers, 1 Official DICE Battlefield Streamer and 2 Battlefield Reddit mods.</p>
-    <p>Our passion for developing community has helped establish the AOD Battlefield servers as some of the most
-        prominent and popular Battlefield servers in North America. Whether you’re here to relax with friends or get
-        PTFO sweaty, you will always find someone to squad up with in Clan AOD.</p>
+    <p>We are first-most a casual gaming group and we accept players of all skill ranges. Respect for your fellow
+        clan-mates, Battlefield players, and having a good time gaming together are what we are all about. We endeavor
+        to create a true community environment of good sportsmanship and absolute fun. Our passion for developing
+        community has helped establish AOD Battlefield servers as some of the most prominent and popular Battlefield
+        servers in North America. Whether you’re here to relax with friends or get PTFO sweaty, you will always find
+        someone to squad up with in Clan AOD.</p>
 </div>
 
 <div class="section-sub-section automenu">
@@ -23,13 +21,14 @@
     <h2 id="servers-and-events">
         Servers and Events
     </h2>
-    <p>The division currently maintains one server for Battlefield 4 (AOD Stomping Grounds) while we gear up for the
-        release of Battlefield 2042. During our weekly Friday Night Battlefield event we may also fire up a Battlefield
-        V server for some shenanigans! If community servers are available in BF2042, you can be sure that the AOD
-        Stomping Grounds will be right there in your server browser.</p>
+    <p>A few times a week, we will run a Portal Server in Battlefield 2042. Our main event nights are Wednesday Evening
+        (Wednesday Night Warfare - BF V) and Friday Evening (AOD FNB - BF2042). Search for AOD STOMPING GROUNDS in your
+        BF2042 Portal Server Browser to find us.
+    </p>
     <p>Our community events range from scheduled matches to impromptu fun; highlighting both the regular game modes and
         those we’ve engineered ourselves. We’re always looking for new and fun ways to keep things interesting. We have
-        also hosted VIP events like hosting the developers at DICE for some good old-fashioned Conquest battles.
+        also hosted VIP events like hosting the developers at DICE for some good old-fashioned Conquest battles and
+        playing with some of the Mods of R/Battlefield2042 and other Battlefield subreddits.
     </p>
 </div>
 <div class="section-sub-section automenu">
@@ -41,12 +40,12 @@
         <li>Be at least 16 years of age</li>
         <li>Be on TeamSpeak whenever you are in-game</li>
         <li>Wear AOD tags (as directed) in any title we support</li>
-        <li>Cook your steaks in the microwave</li>
-        <li>Never put pineapple on pizza</li>
         <li>Contribute to the forums on a monthly basis to maintain activity</li>
+        <li>Cook your steaks in the microwave</li>
+        <li>Be a proud member of No-Pants Nation</li>
     </ul>
 
-    <p>We’ll see YOU on the battlefield!</p>
+    <p>We’ll see <strong><u>YOU</u></strong> on the battlefield!</p>
 
 </div>
 
