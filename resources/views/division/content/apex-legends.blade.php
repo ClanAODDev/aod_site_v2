ddd4c72312ee71648a3dca3687c495a5a1c39700
Chris Deaton <dcdeaton@ncsu.edu>
2022-06-21 12:34:38 -0400

    Add Apex Legends content placeholder

---
+++ resources/views/division/content/apex-legends.blade.php
@@ -0,0 +1,33 @@
+<div class="section-sub-section automenu">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consectetur corporis cumque deleniti deserunt distinctio ducimus earum in laboriosam libero nulla odio, quae quam, ratione repellat reprehenderit repudiandae. Provident, voluptatum!</p>
+</div>
+
+
+
+<div class="section-sub-section automenu">
+    <h2 id="matches-and-events">
+        Matches and Events
+    </h2>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, blanditiis eligendi eum impedit, inventore laborum magni minima minus possimus quaerat repellat totam! Eveniet minus nulla officia recusandae reiciendis repellendus!</p>
+    <ul>
+        <li>Cumque excepturi mollitia, non suscipit veniam vitae. Architecto assumenda cumque delectus, deserunt dicta dignissimos dolorem facilis nam nemo nobis quisquam velit, veniam.</li>
+        <li>Ab aperiam, consequatur consequuntur dicta ea earum eligendi expedita facere fuga inventore ipsa natus nobis non placeat quae, quas qui quisquam! Nihil.</li>
+        <li> Atque dolorem enim expedita nam nostrum saepe vitae voluptatem? Cum excepturi impedit nulla perspiciatis provident quia voluptatem. A ab ipsam quisquam? Laudantium.</li>
+    </ul>
+</div>
+
+<div class="section-sub-section automenu">
+    <h2 id="apply-to-division">
+        Apply to division
+    </h2>
+
+    <p>In order to be eligible for membership in the Battlefront division, you must meet the following criteria:</p>
+    <ul>
+        <li>Be at least 17 years of age</li>
+        <li>Be on Teamspeak whenever you are ingame</li>
+        <li>Include ‘AOD_’ at the start of your Origin name</li>
+        <li>Contribute to the forums at least once a month</li>
+    </ul>
+</div>
+
