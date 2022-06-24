033fe57fd6eaa2361f01c19cfa4b8a23539c8721
Chris Deaton <dcdeaton@ncsu.edu>
2022-06-24 09:05:38 -0400

    Add apex content, blockquote styling

--- resources/views/division/content/apex-legends.blade.php
+++ resources/views/division/content/apex-legends.blade.php
@@ -1,7 +1,16 @@
 <div class="section-sub-section automenu">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consectetur corporis cumque deleniti deserunt distinctio ducimus earum in laboriosam libero nulla odio, quae quam, ratione repellat reprehenderit repudiandae. Provident, voluptatum!</p>
+    <blockquote>
+        <p class="quote">
+            "Our path is made today." </p>
+        <p class="quote-author">BLOODHOUND</p>
+    </blockquote>
+    <p>AOD Apex is primarily a casual gaming group that accepts players from all skill ranges. Having a good time
+        playing together in a drama free environment, helping each other succeed and pushing forward to victory (or
+        horrible, horrible defeat) is what we are all about. We support all Apex game modes - Pubs and Ranked BR, Arenas
+        and Control. We are looking for mature, respectful players who want similar people to play with - regardless of
+        skill. Is that you? Then read on, félagi fighter.
+    </p>
 </div>
-
 
 
 <div class="section-sub-section automenu">
@@ -9,12 +18,12 @@
         Matches and Events
     </h2>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, blanditiis eligendi eum impedit, inventore laborum magni minima minus possimus quaerat repellat totam! Eveniet minus nulla officia recusandae reiciendis repellendus!</p>
-    <ul>
-        <li>Cumque excepturi mollitia, non suscipit veniam vitae. Architecto assumenda cumque delectus, deserunt dicta dignissimos dolorem facilis nam nemo nobis quisquam velit, veniam.</li>
-        <li>Ab aperiam, consequatur consequuntur dicta ea earum eligendi expedita facere fuga inventore ipsa natus nobis non placeat quae, quas qui quisquam! Nihil.</li>
-        <li> Atque dolorem enim expedita nam nostrum saepe vitae voluptatem? Cum excepturi impedit nulla perspiciatis provident quia voluptatem. A ab ipsam quisquam? Laudantium.</li>
-    </ul>
+    <p>We have people playing nightly in our channels - just load into the LFS Channel (Looking for Squad) and form a
+        party for what game mode you are looking for. You can also join a Ranked or Pubs channel and ask for a spot in a
+        squad. It is that simple and easy to join a game with fellow members. We have also run monthly/quarterly
+        division events like Killathon and Apex Tournaments for our members.</p>
+    <p>We are starting up our Apex Division again so we are a smaller group but we are hoping to grow into a larger one.
+        If that interests you, please read on for the membership requirements.</p>
 </div>
 
 <div class="section-sub-section automenu">
@@ -24,10 +33,11 @@
 
     <p>In order to be eligible for membership in the Battlefront division, you must meet the following criteria:</p>
     <ul>
-        <li>Be at least 17 years of age</li>
+        <li>Be at least 16 years of age</li>
         <li>Be on Teamspeak whenever you are ingame</li>
-        <li>Include ‘AOD_’ at the start of your Origin name</li>
-        <li>Contribute to the forums at least once a month</li>
+        <li>Include ‘AOD’ in the beginning of your Origin name, or join our in-game Apex Club</li>
+        <li>Contribute to our community on a monthly basis by engaging on our forums</li>
+        <li>Follow our <a href="https://www.clanaod.net/forums/showthread.php?t=3327">AOD Code of Conduct</a></li>
     </ul>
 </div>
 
