c09e45c6be93f3ee4940242dd19dfa189db211b3
Chris Deaton <dcdeaton@ncsu.edu>
2023-06-17 10:50:31 -0400

    Add Diablo header, content

---
+++ resources/views/division/content/diablo.blade.php
@@ -0,0 +1,19 @@
+<div class="section-sub-section automenu">
+    Content coming soon. Stay tuned!
+</div>
+<div class="section-sub-section automenu">
+    <h2 id="apply-to-division">
+        Apply to division
+    </h2>
+
+    <p>To be eligible for membership in the AOD Diablo Division, you must meet the following criteria:</p>
+    <ul>
+        <li>Be at least 16 years of age</li>
+        <li>Be on Teamspeak whenever you are ingame</li>
+        <li>Wear AOD tags while you are ingame</li>
+        <li>Abide by our Code of Conduct (Don't be toxic)</li>
+        <li>Contribute to the forums once a month to maintain activity</li>
+        <li>Clan AOD is a PC-exclusive gaming community</li>
+    </ul>
+</div>
+
