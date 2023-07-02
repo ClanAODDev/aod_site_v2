774da239f9a8739ba613ab3df3822b99dfc0a610
Chris Deaton <dcdeaton@ncsu.edu>
2023-07-02 17:07:50 -0400

    Planetside updates

--- resources/views/division/content/planetside.blade.php
+++ resources/views/division/content/planetside.blade.php
@@ -1,8 +1,18 @@
 <div class="section-sub-section automenu">
-<p>With an active presence in PlanetSide dating back to 2003, AOD is the top outfit for players looking for a fun, casual gaming environment where they can play alongside dozens of members in an organized fashion. We specialize in operating large forces alongside specialized, cohesive squads in a coordinated manner to control territory. With public platoons running during primetime daily, we'd love for you to hop in and play with us, join us as an outfit member in-game, or even in TeamSpeak as a full-member. Players of all skill levels will find a home where they can play, improve, get involved in the community, and have fun!</p>
-<p>Founded in 1999, the Angels of Death are a large multi-gaming clan. Beyond PlanetSide, we have over 1,000 active members fully supporting over 7 different titles, all with dedicated leadership, officers, and members. When it comes to online gaming communities, you won't find any other organization with the same scale, longevity, organization, and casual environment that has caused many people to call AOD home for decades. We support our operations with our own Forums, TeamSpeak, and Discord.</p>
+    <p>With an active presence in PlanetSide dating back to 2003, AOD is the top outfit for players looking for a fun,
+        casual gaming environment where they can play alongside dozens of members in an organized fashion. We specialize
+        in operating large forces alongside specialized, cohesive squads in a coordinated manner to control territory.
+        With public platoons running during primetime daily, we'd love for you to hop in and play with us, join us as an
+        outfit member in-game, or even in TeamSpeak as a full-member. Players of all skill levels will find a home where
+        they can play, improve, get involved in the community, and have fun!</p>
+    <p><strong>AOD plays as the Terran Republic on Emerald (US East)</strong>.</p>
+    <p>Founded in 1999, the Angels of Death are a large multi-gaming clan. Beyond PlanetSide, we have over 1,000 active
+        members fully supporting over 7 different titles, all with dedicated leadership, officers, and members. When it
+        comes to online gaming communities, you won't find any other organization with the same scale, longevity,
+        organization, and casual environment that has caused many people to call AOD home for decades. We support our
+        operations with our own Forums, TeamSpeak, and Discord.</p>
 
-<p><img src="https://i.imgur.com/yh9xl8R.png" style="
+    <p><img src="https://i.imgur.com/yh9xl8R.png" style="
     padding: 5px;
     filter: drop-shadow(0 0 0.75rem black);
     max-width: 100%;
@@ -15,22 +25,46 @@
         Events
     </h2>
 
-   <h3>Weekly</h3>
-<ul>
-    <li><strong>Infantry Training</strong>: Our Reaper team specilizes in cohesive infantry gameplay. Outside of Ops, they hold a weekly practice where new players can learn higher level infantry play from the basics to the complex, and seasoned players can brush up on their abilities.</li>
-<li><strong>Vehicle Training</strong>: Our Guardian Angel team specilizes in coordinated armor/air gameplay. Outside of Ops, they hold a weekly practice where members can learn the intracties of working well in driver-gunner teams, or holding their own in an armor squad with proper teamwork.</li>
-<li><strong>Platoon/Squad Leading Class</strong>: We offer a comprehensive program that can take a complete newcomer to leading in-game and guide them through all facets of platoon & squad leading. From how to start up a squad, managing a platoon, reading the map, special tactics, and hands on mentorship with real-time feedback to help you improve.</li>
-<li><strong>Off Faction Night</strong>: To switch things up, we'll spend an evening on NC or VS and get a feel for the other factions arsenals and gameplay. We offer NC and VS outfits for our members to join.</li>
-<li><strong>Weekly Operations</strong>: The main event of the week! Every Sunday/Monday, every week, for years, we all get together operate multiple platoons in conjunction with our Reaper & Guardian Special Operation Groups under a force commander, with officers as platoon and squad leads. AOD conducts Ops with scale and coordination that no other outfit can match, and we dominate whatever continent we're on for two hours.</li>
-</ul>
+    <h3>Weekly</h3>
+    <ul>
+        <li><strong>Infantry Training</strong>: Our Reaper team specilizes in cohesive infantry gameplay. Outside of
+            Ops, they hold a weekly practice where new players can learn higher level infantry play from the basics to
+            the complex, and seasoned players can brush up on their abilities.
+        </li>
+        <li><strong>Vehicle Training</strong>: Our Guardian Angel team specilizes in coordinated armor/air gameplay.
+            Outside of Ops, they hold a weekly practice where members can learn the intracties of working well in
+            driver-gunner teams, or holding their own in an armor squad with proper teamwork.
+        </li>
+        <li><strong>Platoon/Squad Leading Class</strong>: We offer a comprehensive program that can take a complete
+            newcomer to leading in-game and guide them through all facets of platoon & squad leading. From how to start
+            up a squad, managing a platoon, reading the map, special tactics, and hands on mentorship with real-time
+            feedback to help you improve.
+        </li>
+        <li><strong>Off Faction Night</strong>: To switch things up, we'll spend an evening on NC or VS and get a feel
+            for the other faction's arsenals and gameplay. We offer NC and VS outfits for our members to join.
+        </li>
+        <li><strong>Weekly Operations</strong>: The main event of the week! Every Sunday/Monday, every week, for years,
+            we all get together operate multiple platoons in conjunction with our Reaper & Guardian Special Operation
+            Groups under a force commander, with officers as platoon and squad leads. AOD conducts Ops with scale and
+            coordination that no other outfit can match, and we dominate whatever continent we're on for two hours.
+        </li>
+    </ul>
 
-<h3>Special</h3>
-<p>Throughout the month we hold special events for our full-members, always changing and new to keep things fun and fresh. From participation in larger community events on live & the Jaeger event server, joint-operations with other outfits, private Jaeger minigames, and construction nights on live. There's always something interesting coming up.</p>
+    <h3>Special</h3>
+    <p>Throughout the month we hold special events for our full-members, always changing and new to keep things fun and
+        fresh. From participation in larger community events on live & the Jaeger event server, joint-operations with
+        other outfits, private Jaeger minigames, and construction nights on live. There's always something interesting
+        coming up.</p>
 
-<p>Take a look at our <a href="https://planetside.clanaod.net/forums/showthread.php?t=242337">complete schedule of events</a>, which includes any upcoming special events.</p>
+    <p>Take a look at our <a href="https://planetside.clanaod.net/forums/showthread.php?t=242337">complete schedule of
+            events</a>, which includes any upcoming special events.</p>
 
-<h2>Full Membership</h2>
-<p>Behind every AOD squad and platoon are our full-members. More than just outfit members in-game, these are members of AOD as a whole. Full-members have access to our private Forums, Discord, and TeamSpeak where all the action happens, as well as the expansive community spanning all our AOD supported games. In-game, full-members also enjoy priority spots in platoons, squad/platoon leading, war assets, and thousands of hours of combined experience in improvement resources.</p>
+    <h2>Full Membership</h2>
+    <p>Behind every AOD squad and platoon are our full-members. More than just outfit members in-game, these are members
+        of AOD as a whole. Full-members have access to our private Forums, Discord, and TeamSpeak where all the action
+        happens, as well as the expansive community spanning all our AOD supported games. In-game, full-members also
+        enjoy priority spots in platoons, squad/platoon leading, war assets, and thousands of hours of combined
+        experience in improvement resources.</p>
 
 </div>
 <div class="section-sub-section automenu">
@@ -38,16 +72,18 @@
         Apply to division
     </h2>
 
-    <p>To join, submit an application to the <a href="https://www.clanaod.net/forums/forms.php?do=form&fid=23">PlanetSide Division</a> and reach out on <a href="https://discord.gg/clanaod">Discord</a>. You can also message any officer in-game or on Discord to get help joining or answer any questions you have beforehand.</p>
+    <p>To join, submit an application to the <a href="https://www.clanaod.net/forums/forms.php?do=form&fid=23">PlanetSide
+            Division</a> and reach out on <a href="https://discord.gg/clanaod">Discord</a>. You can also message any
+        officer in-game or on Discord to get help joining or answer any questions you have beforehand.</p>
 
 
-    <p><strong>Requirements:</strong><br />
-        <ul>
-            <li>Be on TeamSpeak when in-game.</li>
-            <li>Follow our Code of Conduct and Division Regulations.</li>
-            <li>Wear AODR tags in-game.</li>
-            <li>Logon to TeamSpeak once every month to maintain activity.</li>
-        </ul>
+    <p><strong>Requirements:</strong><br/>
+    <ul>
+        <li>Be on TeamSpeak when in-game.</li>
+        <li>Follow our Code of Conduct and Division Regulations.</li>
+        <li>Wear AODR tags in-game.</li>
+        <li>Logon to TeamSpeak once every month to maintain activity.</li>
+    </ul>
     </p>
 
     <p>We hope to see you in-game and on TeamSpeak!</p>
