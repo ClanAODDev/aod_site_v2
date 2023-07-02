9cbd7230192ac29a3c309e105c68df6cb0509ac9
Chris Deaton <dcdeaton@ncsu.edu>
2023-07-02 17:02:37 -0400

    Battlebit content update

---
+++ resources/views/division/content/battlebit-remastered.blade.php
@@ -0,0 +1,49 @@
+<div class="section-sub-section automenu">
+    <p>Thank you for showing interest in the BattleBit Remastered division of the Angels of Death. We are a casual
+        gaming community with players of every skill level, who focus on teamwork, sportsmanship, and maintaining a
+        positive upbeat gaming environment.</p>
+
+    <p>Whether we are playing objective, operating armor or attempting to destroy an entire city block with C4 and
+        sledgehammers, with Clan AOD you will always have people to squad up with and have a great time.</p>
+</div>
+
+
+<div class="section-sub-section automenu">
+    <h2 id="launch-trailer">BattleBit Remastered Launch Trailer</h2>
+
+    <iframe class="youtube-embed" src="https://www.youtube.com/embed/AO8cokSpALA"
+            title="BattleBit Remastered Launch Trailer" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+    ></iframe>
+</div>
+
+<div class="section-sub-section automenu">
+    <h2 id="news-updates">
+        News and Updates
+    </h2>
+    <p>In the AOD BattleBit Remastered discord channel we post all news, updates and developer releases nearly as they
+        are released. Follow our discord to keep up with all of your BattleBit Remastered news.</p>
+</div>
+
+<div class="section-sub-section automenu">
+    <h2 id="servers-and-events">Servers and Events</h2>
+    <p>As soon as Oki games releases community servers Angels of Death will be hosting our own server. Featuring our
+        favorite maps and game types as well as community events.</p>
+</div>
+
+<div class="section-sub-section automenu">
+    <h2 id="apply-to-division">Apply to division</h2>
+    <p>In order to be eligible for membership in the Battlebit Remastered division, you must meet the following
+        criteria:</p>
+
+    <ul>
+        <li>Be at least 16 years old</li>
+        <li>Join us in Teamspeak whenever playing</li>
+        <li>Proudly wear your AOD clan tags in game</li>
+        <li>Be friendly and respectful of all players</li>
+    </ul>
+
+</div>
+
+
