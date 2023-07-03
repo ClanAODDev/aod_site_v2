f2d55e73804b728795d3cdcc28487c15a251e40f
Chris Deaton <dcdeaton@ncsu.edu>
2023-07-02 20:51:57 -0400

    Add diablo content

--- resources/views/division/content/diablo.blade.php
+++ resources/views/division/content/diablo.blade.php
@@ -1,5 +1,24 @@
 <div class="section-sub-section automenu">
-    <p>Content coming soon. Stay tuned!</p>
+    <p>The Diablo Division is one of AOD’s newer divisions which was created with the launch of Diablo 4. The Diablo
+        Division is growing and we have a range of players from those working their first characters through the Story
+        line to those grinding levels to reach 100.</p>
+    <p>You can always find someone to run Hell Tides, Capstones, Grim
+        Favors, World Boss/Legion Events as well as those who are grinding NighMare dungeons for XP, Gear and Glyph XP.
+        With all the different ways one can build their characters, it’s great to have a group of players to bounce
+        ideas off and try new things with.</p>
+</div>
+<div class="section-sub-section automenu">
+    <h2>
+        Seasons, Battlepasses and PvP
+    </h2>
+
+    <p>We are looking forward to the upcoming seasons and to see how Blizzard plans to introduce them. What kind of
+        adventures will they have? Plus there is the Battle Pass that they have promised to introduce as well. What
+        will that give us? Come join us and let’s explore the future of Sanctuary together. Most of the Division
+        plays PvE, but once the Seasons start off and get going, we intend to start delving into the world of PvP in
+        Diablo. PvP will offer a break from grinding new seasonal characters and enjoy a different aspect of game
+        play Diablo offers.
+    </p>
 </div>
 <div class="section-sub-section automenu">
     <h2 id="apply-to-division">
