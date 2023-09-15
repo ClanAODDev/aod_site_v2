17b410dac7cb75b13da0f24ac7c4e8a024cfa410
Chris Deaton <dcdeaton@ncsu.edu>
2023-09-14 21:30:37 -0400

    CoD updates

--- resources/views/division/content/call-of-duty.blade.php
+++ resources/views/division/content/call-of-duty.blade.php
@@ -1,42 +1,35 @@
 <div class="section-sub-section automenu">
 
-    <p>Welcome, Soldier, and thank you for your interest in the Angels of Death!</p>
+    <p>Welcome, Soldier, and thank you for your interest in the Angels of Death Call of Duty Division!</p>
 
-    <p>The Angels of Death – Call of Duty Division supports Warzone, Vanguard, Modern Warfare, and Cold War. Our members
-        are spread out over multiple game modes. We have members actively playing battle royale, multiplayer and
-        zombies. We’re a gaming community that knows how to win but prefers good people over skill. Our goal is to
-        create a diverse, friendly, and non-toxic environment for all Angels of Death members to play the games they
-        enjoy. We communicate, we play clean, and we play as a team.</p>
+    <p>The Angels of Death – Call of Duty Division officially supports Warzone 2, Modern Warfare 2, and will support the upcoming Modern Warfare 3. Our members are spread out over multiple game modes. We have members actively playing battle royale, DMZ, and multiplayer. And while we do not officially support older titles we often have members go back and experience older content (especially when it comes to zombies).</p>
 
-    <p>The Call of Duty Division is home to members from all over the world with varying skill levels. No matter your
-        time zone or level of experience, there is always someone online to squad up with. We are united by our love for
-        gaming and the fun, mature, judgment-free atmosphere we have created. If you’re looking for a community with
-        members that play with honor and respect, you're in the right place.</p>
+    <p>We’re a gaming community that knows how to win but prefers good people over skill. Our goal is to create a diverse, friendly, and non-toxic environment for all Angels of Death members to play the games they enjoy. We communicate, we play clean, and we play as a team.</p>
+
+    <p>The Call of Duty Division is home to members from all over the world with varying skill levels. We are united by our love for gaming and the fun, mature, judgment-free atmosphere we have created. If you’re looking for a community with members that play with honor and respect, you're in the right place.</p>
 </div>
 
 <div class="section-sub-section automenu">
     <h2 id="events-&amp;-badges">
-        Events &amp; Badges
+        Events &amp;amp; Badges
     </h2>
 
-    <p>The Call of Duty Division hosts weekly events for all clan supported Call of Duty games as well as many others.
-        The Officer Team makes it our priority to keep the members engaged with custom events and challenges.</p>
+    <p>The Call of Duty Division hosts weekly events for all clan supported Call of Duty games as well as many others. The Officer Team makes it our priority to keep the members engaged with custom events and challenges.</p>
     <ul>
-        <li>Pub Nights on Friday and Saturday</li>
-        <li>CDL Playlist Sundays</li>
-        <li>Warzone Kill-a-Thon &amp; Plunder-Thon Events</li>
-        <li>Kill Count &amp; Objective Challenges</li>
-        <li>Poker &amp; Skribbl.io Game Nights</li>
+        <li>Pub Nights - Normally on a monthly basis</li>
+        <li>Warzone Kill-a-Thon & Plunder-Thon Events</li>
+        <li>Assorted Weekly Events</li>
+        <li>Poker & Skribbl.io Game Nights</li>
         <li>Custom Games, Tournaments, and tons more!</li>
     </ul>
-    <p>There are also AOD Exclusive Forum Badges and Awards that any member can earn through special events, in-game
-        challenges, and tournaments.</p>
+    <p>There are also AOD Exclusive Forum Badges and Awards that any member can earn through special events, in-game challenges, and tournaments.</p>
     <ul>
-        <li>Seasoned Officer (Prestige Progression)</li>
+        <li>Dedicated Soldier (Prestige Progression)</li>
         <li>Gilded Guns (Gun Camo Challenges)</li>
         <li>Answered The Call (Participation in Quarterly Events)</li>
-        <li>Gunslinger (Quarterly Tournament Champions)</li>
-        <li>Verdansk Veteran (Warzone Wins)</li>
+        <li>Second to None (Winning Quarterly Events)</li>
+        <li>Royale Champion (Warzone Wins)</li>
+        <li>Member of the Quarter (Being a good member)</li>
     </ul>
 </div>
 
@@ -45,13 +38,13 @@
         Apply to division
     </h2>
 
-    <p>To be eligible for membership in the Angels of Death – Call of Duty Division, you must meet and agree to the
-        following requirements;</p>
+    <p>To be eligible for membership in the Angels of Death – Call of Duty Division, you must meet and agree to the following requirements.
+    </p>
     <ul>
-        <li>Play Call of Duty on PC</li>
-        <li>Be at least 18 years of age</li>
-        <li>Contribute to the forums every month to stay active</li>
-        <li>Be on TeamSpeak when playing Call of Duty</li>
-        <li>Wear the AOD tags when playing Call of Duty</li>
+        <li>Play Call of Duty (preferably on PC, just makes connecting to TeamSpeak easier)</li>
+        <li>Be at least 18 years of age.</li>
+        <li>Be on TeamSpeak when playing Call of Duty.</li>
+        <li>Be on TeamSpeak at least once monthly to stay active.</li>
+        <li>Wear the AOD tags when playing Call of Duty.</li>
     </ul>
 </div>
