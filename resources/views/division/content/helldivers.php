f53f9d2ef1ed0853623bcb3cc8fef5127eec852e
Chris Deaton <dcdeaton@ncsu.edu>
2024-03-22 21:35:44 -0400

    Add Helldivers page

---
+++ resources/views/division/content/helldivers.php
@@ -0,0 +1,48 @@
+<div class="section-sub-section automenu">
+    <strong>Freedom. Peace. Democracy.</strong>
+    <p>Enlist in the AOD Helldivers and join the fight for freedom across a hostile galaxy.
+        We are a casual gaming group, and we accept players of all skill ranges. We don’t require meta builds or force a
+        certain playstyle. We only ask two things: respect for your fellow Helldivers and have a good time gaming
+        together. We endeavor to create a true community environment of good sportsmanship and absolute fun.</p>
+</div>
+
+
+<div class="section-sub-section automenu">
+    <h2 id="helldiver-scouts">Helldiver Scouts</h2>
+    <p>Our veteran Helldivers are here to assist your climb to Skull Admiral. Need help with Helldive difficulty
+        missions? We got your back! Running low on Super Samples? We’ll show you where they are and help you extract
+        with full Democracy! Need to find the bathroom on your Super Destroyer? Our highly trained AOD Helldiver Scouts
+        are here to help you spread LIBER-TEA!</p>
+
+</div>
+
+<div class="section-sub-section automenu">
+    <h2 id="death-from-above">
+        Death From Above
+    </h2>
+    <p>Don’t waste any more time gaming with random cannon fodder. Join the AOD Helldivers and you’ll always have voice
+        comms and coordinated strategy with your squad. Never fight alone. Become the ultimate dispenser of Freedom!</p>
+    <iframe class="youtube-embed" src="https://www.youtube.com/watch?v=oD3pxbG9YYI"
+            title="HELLDIVERS: DEATH FROM ABOVE" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+    ></iframe>
+</div>
+
+<div class="section-sub-section automenu">
+    <h2 id="apply-to-division">Apply to division</h2>
+    <p>In order to be eligible for membership in the Battlebit Remastered division, you must meet the following
+        criteria:</p>
+
+    <ul>
+        <li>Be at least 18 years old</li>
+        <li>Have a microphone and be in TeamSpeak whenever you are playing</li>
+        <li>Be active and engaged to maintain your membership</li>
+        <li>Have a Discord account and join our Discord server</li>
+        <li>Be a fan of extreme meme activity and gif wars</li>
+        <li>Pants are optional. Liber-tea is mandatory</li>
+    </ul>
+
+</div>
+
+
