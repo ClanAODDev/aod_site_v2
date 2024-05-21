f94847737a42bfa7dd4b2fb2d9095af915e3b359
Chris Deaton <dcdeaton@ncsu.edu>
2024-05-21 19:26:58 -0400

    Add XDefiant content Content coming soon

---
+++ resources/views/division/content/xdefiant.blade.php
@@ -0,0 +1,19 @@
+<div class="section-sub-section automenu">
+    <p>Content coming soon!</p>
+</div>
+
+<div class="section-sub-section automenu">
+    <h2 id="apply-to-division"> Apply to division </h2>
+    <p>In order to be eligible for membership in the XDefiant division, you must meet the following
+        criteria:</p>
+
+    <ul>
+        <li>Be at least 16 years of age</li>
+        <li>Be on TeamSpeak whenever you are in-game</li>
+        <li>Wear AOD tags (as directed) in any title we support</li>
+        <li>Be friendly to all other members and server players</li>
+    </ul>
+
+</div>
+
+
