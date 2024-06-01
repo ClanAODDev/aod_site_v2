9e5a1dea49f27e9096b549f9dd3f8b3f1cd284c8
Chris Deaton <dcdeaton@ncsu.edu>
2024-05-31 21:19:27 -0400

    Add XD content

--- resources/views/division/content/xdefiant.blade.php
+++ resources/views/division/content/xdefiant.blade.php
@@ -1,19 +1,48 @@
 <div class="section-sub-section automenu">
-    <p>Content coming soon!</p>
+
+    <p>Welcome, Soldier, and thank you for your interest in the Angels of Death XDefiant Division!</p>
+
+    <p>The Angels of Death – XDefiant Division ).</p>
+
+    <p>We’re a gaming community that knows how to win but prefers good people over skill. Our goal is to create a diverse, friendly, and non-toxic environment for all Angels of Death members to play the games they enjoy. We communicate, we play clean, and we play as a team.</p>
+
+    <p>The XDefiant Division is home to members from all over the world with varying skill levels. We are united by our love for gaming and the fun, mature, judgment-free atmosphere we have created. If you’re looking for a community with members that play with honor and respect, you're in the right place.</p>
 </div>
 
 <div class="section-sub-section automenu">
-    <h2 id="apply-to-division"> Apply to division </h2>
-    <p>In order to be eligible for membership in the XDefiant division, you must meet the following
-        criteria:</p>
+    <h2 id="events-and-badges">
+        Events & Badges
+    </h2>
 
+    <p>The XDefiant Division Will host weekly events for all members. The Officer Team makes it our priority to keep the members engaged with custom events and challenges.</p>
     <ul>
-        <li>Be at least 16 years of age</li>
-        <li>Be on TeamSpeak whenever you are in-game</li>
-        <li>Wear AOD tags (as directed) in any title we support</li>
-        <li>Be friendly to all other members and server players</li>
+        <li>Pub Nights - Normally on a monthly basis</li>
+        <li>Assorted Weekly Events</li>
+        <li>Poker & Skribbl.io Game Nights</li>
+        <li>Custom Games, Tournaments, and tons more!</li>
     </ul>
-
+    <p>There are also AOD Exclusive Forum Badges and Awards that any member can earn through special events, in-game challenges, and tournaments.</p>
+    <ul>
+        <li>Phantom core Soldier (Lvl 200)</li>
+        <li>Guns of glory (Gun Camo Challenges)</li>
+        <li>Wildland hunters (Participation in Quarterly Events)</li>
+        <li>Project Titan (Winning Quarterly Events)</li>
+        <li>Master of the Shadows (Being the best officer)</li>
+    </ul>
 </div>
 
+<div class="section-sub-section automenu">
+    <h2 id="apply-to-division">
+        Apply to division
+    </h2>
 
+    <p>To be eligible for membership in the Angels of Death – XDefiant Division, you must meet and agree to the following requirements.
+    </p>
+    <ul>
+        <li>Play XDefiant (preferably on PC, just makes connecting to TeamSpeak easier)</li>
+        <li>Be at least 16 years of age.</li>
+        <li>Be on TeamSpeak when playing XDefiant.</li>
+        <li>Be on TeamSpeak at least once monthly to stay active.</li>
+        <li>Wear the AOD tags when playing XDefiant (Change name to AOD_ on Uplay).</li>
+    </ul>
+</div>
