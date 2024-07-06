aa10703fd4d666511188de5df0766c813c5de627
Chris Deaton <dcdeaton@ncsu.edu>
2024-07-06 11:10:12 -0400

    OH content update

--- resources/views/division/content/once-human.blade.php
+++ resources/views/division/content/once-human.blade.php
@@ -1,8 +1,6 @@
 
 <div class="section-sub-section automenu">
     <p>Explore a hauntingly beautiful open world twisted by otherworldly energies. Unleash your inner Deviant: Master unique abilities that defy the laws of nature.</p>
-</div>
-<div class="section-sub-section automenu">
 	<p>Scavenge, craft, and build to survive in a hostile environment. Forge alliances or fight for dominance in a world where humanity is reborn. Embrace the mysteries of the unraveling and shape the fate of a shattered world. This is not just a game. It's a testament to the enduring spirit of humanity.</p>
 </div>
 
@@ -10,19 +8,14 @@
     <h2 id="oh-teaser">
         Once Human
     </h2>
+
+    <p>Once Human throws you into a captivating open world ravaged by an apocalyptic event. You're not just a survivor, but a "Deviant" – wielding extraordinary abilities fueled by mysterious energies. In this open world survival MMO you will explore eerie landscapes, build shelters, craft gear, and fight mutated creatures. Team up with friends to conquer challenges and face intense PvP battles. Unravel the secrets of this shattered world and define the future of humanity as you navigate complex moral choices and uncover the truth behind the apocalyptic event.</p>
+    <p>Embrace the unknown. Become the Deviant. Once Human. Are you ready to evolve?</p>
+
     <iframe class="youtube-embed" src="https://www.youtube.com/embed/XTwOAAGhLCk"
             title="Once Human | Trailer" frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen></iframe>
-</div>
-
-<div class="section-sub-section automenu">
-
-    <p>Once Human throws you into a captivating open world ravaged by an apocalyptic event. You're not just a survivor, but a "Deviant" – wielding extraordinary abilities fueled by mysterious energies. In this open world survival MMO you will explore eerie landscapes, build shelters, craft gear, and fight mutated creatures. Team up with friends to conquer challenges and face intense PvP battles. Unravel the secrets of this shattered world and define the future of humanity as you navigate complex moral choices and uncover the truth behind the apocalyptic event.
-    </p>
-</div>
-<div class="section-sub-section automenu">
-Embrace the unknown. Become the Deviant. Once Human. Are you ready to evolve?
 </div>
 
 <div class="section-sub-section automenu">
