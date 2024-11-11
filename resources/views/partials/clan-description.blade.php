3c19378b2d654cd2b16e37578efae55c94fdde56
Chris Deaton <dcdeaton@ncsu.edu>
2024-11-11 11:24:48 -0500

    Drop description template

--- resources/views/partials/clan-description.blade.php
+++
@@ -1,2 +0,0 @@
-The Angels of Death is a community of players founded in 1999 based on a core set of conduct that
-aims to promote decency and provide a comfortable environment to play with thousands of other like-minded members.
