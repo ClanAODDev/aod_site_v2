e42b9a255604158430128d734646b139ec69c1d5
Chris Deaton <dcdeaton@ncsu.edu>
2025-01-26 04:17:39 -0500

    Laravel upgrade 9>11

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -18,10 +18,6 @@ class Handler extends ExceptionHandler
 
     public function report(Throwable $exception)
     {
-        if (app()->bound('sentry') && $this->shouldReport($exception)) {
-            app('sentry')->captureException($exception);
-        }
-
         parent::report($exception);
     }
 
