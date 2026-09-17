2f4172b35a3570d038205ef6519c1bbe14066a3b
Guybrush <me@chrisdeaton.dev>
2026-09-17 03:24:23 -0400

    Hero: restore play button clickability at rest

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -77,7 +77,7 @@ export default function Home({ discord, twitch, highlightedEvent, showTwitchLive
                 isChristmas={isChristmas}
             />
 
-            <div className="h-[55vh]" />
+            <div className="pointer-events-none h-[55vh]" />
 
             <Nav startsUnstuck />
 
